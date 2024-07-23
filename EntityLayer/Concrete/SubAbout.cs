1a10fb872ede99f9e22c3b47280c66614673cf45
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-23 17:59:11 +0300

    Add project files.

---
+++ EntityLayer/Concrete/SubAbout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class SubAbout
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Image { get; set; }
+
+        public bool Status { get; set; }=true;
+    }
+}
