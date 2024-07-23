126aaf87874101cc6d4bfd0872aedc3e6e35b49b
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-24 00:14:11 +0300

    Ana Sayfa dinamik hale getirildi.

--- Traversal.Web/ViewComponents/Default/_PopularDestinations.cs
+++ Traversal.Web/ViewComponents/Default/_PopularDestinations.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Absract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
 
 namespace Traversal.Web.ViewComponents.Default
 {
     public class _PopularDestinations:ViewComponent
     {
+        private readonly IDestinationRepository _destinationRepository;
+
+        public _PopularDestinations(IDestinationRepository destinationRepository)
+        {
+            _destinationRepository = destinationRepository;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var result= _destinationRepository.GetAll().ToList();
+            return View(result);
         }
     }
 }
