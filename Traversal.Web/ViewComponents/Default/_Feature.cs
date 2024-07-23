0a141cbdfa4033e07c461d3e84aed9afd133d816
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-24 00:53:03 +0300

    #

---
+++ Traversal.Web/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Web.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        private readonly FeatureService _featureService;
+
+        public _Feature(FeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result=_featureService.GetAll().ToList();
+            return View(result);
+        }
+    }
+}
---
+++ Traversal.Web/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Web.ViewComponents.Default
+{
+
+
+    public class _Statistics : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public _Statistics(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _db.Destinations.Count();
+            ViewBag.v2= _db.Guides.Count();
+          
+
+            return View();
+        }
+    }
+}
