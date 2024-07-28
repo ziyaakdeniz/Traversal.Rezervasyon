b21e2aa96c81ba21483c061c4538cde184708609
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-28 11:18:40 +0300

    #

--- DataAccessLayer/ApplicationDbContext.cs
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext<AppUser,AppRole,int>
     {
         public ApplicationDbContext()
         {
@@ -41,6 +42,7 @@ namespace DataAccessLayer
 
         public virtual DbSet<Comment> Comments { get; set; }
 
+        
 
     }
 }
---
+++ Traversal.Web/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+        [HttpGet]
+        public IActionResult SingUp()
+        {
+            return View();
+        }
+        [HttpPost]
+
+
+        //public IActionResult SingUp()
+        //{
+
+        //    return View();
+        //}
+
+
+
+        [HttpGet]
+        public IActionResult SingIn()
+        {
+            return View();
+        }
+
+        
+    }
+}
