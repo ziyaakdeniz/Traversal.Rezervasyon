6596a7a0803c8f0e698d85818a920a10fb25eeaf
ziya akdeniz <ziaakdeniz@gmail.com>
2024-08-01 21:18:09 +0300

    #

--- Traversal.Web/Areas/Member/Controllers/ReservationController.cs
+++ Traversal.Web/Areas/Member/Controllers/ReservationController.cs
@@ -1,28 +1,47 @@
 ﻿using BusinessLayer.Abstact;
 using EntityLayer.Concrete;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Traversal.Web.Areas.Member.Controllers
 {
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
+
 
     public class ReservationController : Controller
     {
         private readonly IDestinationService _destinationService;
         private readonly IReservationService _reservationService;
+        private readonly Microsoft.AspNetCore.Identity.UserManager<AppUser> _userManager;
 
-        public ReservationController(IDestinationService destinationService, IReservationService reservationService)
+        public ReservationController(IDestinationService destinationService, IReservationService reservationService, Microsoft.AspNetCore.Identity.UserManager<AppUser> userManager)
         {
             _destinationService = destinationService;
             _reservationService = reservationService;
+            _userManager = userManager;
+
         }
 
         public IActionResult MyCurrentReservation()
         {
+
             return View();
         }
+
+        public async Task<IActionResult> MyApprovalReservation()
+        { 
+            var result=await _userManager.FindByNameAsync(User.Identity.Name);
+            
+           ViewBag.Id=result.Id;
+            var reservations =  _reservationService.GetAll().Where(x => x.status.StartsWith("Onay") && x.AppUserId==result.Id).ToList();
+
+            return View(reservations);
+        }
+
 
         public IActionResult MyOldReservation()
         {
@@ -32,23 +51,32 @@ namespace Traversal.Web.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
-            List<SelectListItem> values = (from x in _destinationService.GetAll().ToList()
-                                           select new SelectListItem
-                                           {
-                                               Text = x.City,
-                                               Value = x.Id.ToString()
-                                           }).ToList();
-            ViewBag.result  = values;
+            //List<SelectListItem> values = (from x in _destinationService.GetAll()
+            //                               select new SelectListItem
+            //                               {
+            //                                   Text = x.City,
+            //                                   Value = x.Id.ToString()
+            //                               }).ToList();
+            List<SelectListItem> values = _destinationService.GetAll().Select(x => new SelectListItem
+            {
+                Text = x.City,
+                Value = x.Id.ToString()
+            }).ToList();
+
+            ViewBag.result = values;
             return View();
         }
 
-        [HttpGet]
+        [HttpPost]
         public IActionResult NewReservation(Reservation reservation)
         {
+            reservation.status = "Onay Bekliyor";
             reservation.AppUserId = 3;
             _reservationService.Add(reservation);
 
             return RedirectToAction("MyCurrentReservation");
         }
+
+        
     }
 }
--- Traversal.Web/Program.cs
+++ Traversal.Web/Program.cs
@@ -63,10 +63,13 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-  );
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//      name: "areas",
+//      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+//    );
+//});
 
 
 
